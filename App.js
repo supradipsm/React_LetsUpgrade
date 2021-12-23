7861e9895e2556dd928e33779c915c81edffe1ed
supradipsm <96065501+supradipsm@users.noreply.github.com>
2021-12-24 02:56:12 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Assignment1 from "./Assignment1.js"
+
+const App=()=>{
+    return (
+    <div style={{textAlign:"center"}}>
+    <h1 >My New React Application</h1>
+    <Assignment1/>
+   
+    </div>
+    )
+}
+
+export default App
---
+++ Assignment1.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Button from "./Button"
+const Assignment1=()=>{
+    return(<div>
+    <Button buttonText="My Facebook"/>
+    <Button buttonText="My LinkedIn"/>
+    <Button buttonText="My Github"/>
+    </div>
+    )
+}
+export default Assignment1
